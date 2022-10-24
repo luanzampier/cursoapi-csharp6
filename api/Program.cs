54e5b9a9daf05df7b7992cac9de63500e44c2ff7
Luan Zampier Rezende <luanzampier@live.com>
2022-10-24 14:24:07 -0300

    passando parametros pelo header

--- api/Program.cs
+++ api/Program.cs
@@ -4,5 +4,8 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.MapPost("/", () => new {Name = "Luan Zampier", Age = 26});
+app.MapGet("/AddHeader", (HttpResponse response) => {
+    response.Headers.Add("teste", "Luan");
+} );
 
 app.Run();
